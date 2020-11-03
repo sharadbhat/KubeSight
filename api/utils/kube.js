7051be7727fb1da4090b73858e18c1a2f3fee73b
sharadbhat <sharad.mbhat@gmail.com>
2020-11-04 03:43:45 +0530

    kube conn

---
+++ api/utils/kube.js
@@ -0,0 +1,60 @@
+const k8s = require("@kubernetes/client-node");
+
+let coreV1API = null;
+let appsV1API = null;
+let batchV1API = null;
+let batchV1Beta1API = null;
+let rbacAuthorizationV1API = null;
+let storageV1API = null;
+
+let initConn = new Promise((resolve, reject) => {
+  const kc = new k8s.KubeConfig();
+  kc.loadFromDefault();
+
+  coreV1API = kc.makeApiClient(k8s.CoreV1Api);
+  appsV1API = kc.makeApiClient(k8s.AppsV1Api);
+  batchV1API = kc.makeApiClient(k8s.BatchV1Api);
+  batchV1Beta1API = kc.makeApiClient(k8s.BatchV1beta1Api);
+  rbacAuthorizationV1API = kc.makeApiClient(k8s.RbacAuthorizationV1Api);
+  storageV1API = kc.makeApiClient(k8s.StorageV1Api);
+
+  if (coreV1API) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+getCoreV1API = () => {
+  return coreV1API;
+};
+
+getAppsV1API = () => {
+  return appsV1API;
+};
+
+getBatchV1API = () => {
+  return batchV1API;
+};
+
+getBatchV1Beta1API = () => {
+  return batchV1Beta1API;
+};
+
+getRbacAuthorizationV1API = () => {
+  return rbacAuthorizationV1API;
+};
+
+getStorageV1API = () => {
+  return storageV1API;
+};
+
+module.exports = {
+  initConn,
+  getCoreV1API,
+  getAppsV1API,
+  getBatchV1API,
+  getBatchV1Beta1API,
+  getRbacAuthorizationV1API,
+  getStorageV1API,
+};
