20c55d6284e85897d7a1efa3169123c59db5eed1
sharadbhat <sharad.mbhat@gmail.com>
2020-11-06 17:47:02 +0530

    two pages

--- client/src/pages/subpages/workloads/CronjobsList.jsx
+++ client/src/pages/subpages/workloads/CronjobsList.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import { message, Tag, Tooltip, Collapse } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { v4 as uuid } from "uuid";
+import cronstrue from "cronstrue";
+
+// Components
+import DataTable from "../../../components/DataTable";
 
 // Utils
 import { Context } from "../../../utils/Context";
@@ -6,14 +14,151 @@ import { Context } from "../../../utils/Context";
 class CronjobsList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [],
+    };
+
+    this.suspendColors = {
+      True: "orange",
+      False: "green",
+    };
+
+    this.columns = [
+      {
+        title: "Name",
+        dataIndex: ["metadata", "name"],
+        key: uuid(),
+        sorter: (a, b) => a.metadata.name.localeCompare(b.metadata.name),
+        sortDirections: ["descend", "ascend"],
+      },
+      {
+        title: "Namespace",
+        dataIndex: ["metadata", "namespace"],
+        key: uuid(),
+      },
+      {
+        title: "Labels",
+        dataIndex: ["metadata", "labels"],
+        key: uuid(),
+        render: (labelObject) => {
+          let labelList = [];
+          for (const label in labelObject) {
+            if (labelObject.hasOwnProperty(label)) {
+              labelList.push(`${label}: ${labelObject[label]}`);
+            }
+          }
+
+          if (labelList.length > 0) {
+            return (
+              <Collapse>
+                <Collapse.Panel header="View">
+                  {labelList.map((label) => {
+                    return <Tag>{label}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+      {
+        title: "Schedule",
+        dataIndex: ["spec", "schedule"],
+        key: uuid(),
+        render: (schedule) => {
+          return (
+            <Tooltip title={cronstrue.toString(schedule)}>{schedule}</Tooltip>
+          );
+        },
+      },
+      {
+        title: "Suspend",
+        dataIndex: ["spec", "suspend"],
+        key: uuid(),
+        render: (suspend) => {
+          let suspendVal = suspend ? "True" : "False";
+          return <Tag color={this.suspendColors[suspendVal]}>{suspendVal}</Tag>;
+        },
+      },
+      {
+        title: "Active",
+        dataIndex: ["status", "active"],
+        key: uuid(),
+        render: (activeList) => {
+          if (activeList) {
+            return activeList.length;
+          }
+          return 0;
+        },
+      },
+      {
+        title: "Last Schedule",
+        dataIndex: ["status", "lastScheduleTime"],
+        key: uuid(),
+        render: (lastScheduleTime) => {
+          return (
+            <Tooltip
+              title={moment(lastScheduleTime).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(lastScheduleTime).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+      {
+        title: "Age",
+        dataIndex: ["metadata", "creationTimestamp"],
+        key: uuid(),
+        sorter: (a, b) =>
+          moment(b.metadata.creationTimestamp) -
+          moment(a.metadata.creationTimestamp),
+        sortDirections: ["descend", "ascend"],
+        render: (creationTimestamp) => {
+          return (
+            <Tooltip
+              title={moment(creationTimestamp).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(creationTimestamp).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+    ];
   }
 
   componentDidMount = () => {
     this.context.setHeader("Cron Jobs");
+    this.getCronJobs();
+  };
+
+  getCronJobs = async () => {
+    try {
+      let serverResponse = await axios.get(
+        `/workload/${this.context.state.namespace}/get-cron-jobs`
+      );
+      if (serverResponse.status === 200) {
+        this.setState({
+          data: serverResponse.data.response.body.cronJobs,
+        });
+      } else {
+        console.log("Error occurred");
+        message.error("Error occurred");
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+      message.error("Error occurred");
+    }
   };
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <DataTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
   }
 }
 
--- client/src/pages/subpages/workloads/JobsList.jsx
+++ client/src/pages/subpages/workloads/JobsList.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import { message, Tag, Tooltip, Collapse, Popover, Descriptions } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { v4 as uuid } from "uuid";
+import cronstrue from "cronstrue";
+
+// Components
+import DataTable from "../../../components/DataTable";
 
 // Utils
 import { Context } from "../../../utils/Context";
@@ -6,14 +14,162 @@ import { Context } from "../../../utils/Context";
 class JobsList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [],
+      labels: [],
+      labelsModalVisible: false,
+      images: [],
+      imagesModalVisible: false,
+    };
+
+    this.columns = [
+      {
+        title: "Name",
+        dataIndex: ["metadata", "name"],
+        key: uuid(),
+        sorter: (a, b) => a.metadata.name.localeCompare(b.metadata.name),
+        sortDirections: ["descend", "ascend"],
+      },
+      {
+        title: "Namespace",
+        dataIndex: ["metadata", "namespace"],
+        key: uuid(),
+      },
+      {
+        title: "Labels",
+        dataIndex: ["metadata", "labels"],
+        key: uuid(),
+        render: (labelObject) => {
+          let labelList = [];
+          for (const label in labelObject) {
+            if (labelObject.hasOwnProperty(label)) {
+              labelList.push(`${label}: ${labelObject[label]}`);
+            }
+          }
+
+          if (labelList.length > 0) {
+            return (
+              <Collapse>
+                <Collapse.Panel header="View">
+                  {labelList.map((label) => {
+                    return <Tag>{label}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+      {
+        title: "Pods",
+        dataIndex: ["status"],
+        key: uuid(),
+        render: (statusObject) => {
+          let total =
+            (statusObject["active"] || 0) +
+            (statusObject["succeeded"] || 0) +
+            (statusObject["failed"] || 0);
+          return (
+            <Popover
+              placement="right"
+              content={
+                <Descriptions bordered size="small" column={1}>
+                  <Descriptions.Item label={<Tag color="green">Active</Tag>}>
+                    {statusObject["active"] || 0}
+                  </Descriptions.Item>
+                  <Descriptions.Item label={<Tag color="blue">Succeeded</Tag>}>
+                    {statusObject["succeeded"] || 0}
+                  </Descriptions.Item>
+                  <Descriptions.Item label={<Tag color="red">Failed</Tag>}>
+                    {statusObject["failed"] || 0}
+                  </Descriptions.Item>
+                </Descriptions>
+              }
+            >{`${statusObject["active"] || 0} / ${total}`}</Popover>
+          );
+        },
+      },
+      {
+        title: "Age",
+        dataIndex: ["metadata", "creationTimestamp"],
+        key: uuid(),
+        sorter: (a, b) =>
+          moment(b.metadata.creationTimestamp) -
+          moment(a.metadata.creationTimestamp),
+        sortDirections: ["descend", "ascend"],
+        render: (creationTimestamp) => {
+          return (
+            <Tooltip
+              title={moment(creationTimestamp).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(creationTimestamp).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+      {
+        title: "Images",
+        dataIndex: ["spec", "template", "spec", "containers"],
+        key: uuid(),
+        render: (containersList) => {
+          console.log(containersList);
+
+          let imageList = [];
+          containersList.forEach((container) => {
+            imageList.push(container.image);
+          });
+
+          if (imageList.length > 0) {
+            return (
+              <Collapse>
+                <Collapse.Panel header="View">
+                  {imageList.map((image) => {
+                    return <Tag>{image}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+    ];
   }
 
   componentDidMount = () => {
     this.context.setHeader("Jobs");
+    this.getJobs();
+  };
+
+  getJobs = async () => {
+    try {
+      let serverResponse = await axios.get(
+        `/workload/${this.context.state.namespace}/get-jobs`
+      );
+      if (serverResponse.status === 200) {
+        this.setState({
+          data: serverResponse.data.response.body.jobs,
+        });
+      } else {
+        console.log("Error occurred");
+        message.error("Error occurred");
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+      message.error("Error occurred");
+    }
   };
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <DataTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
   }
 }
 
