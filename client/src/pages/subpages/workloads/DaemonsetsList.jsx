449afc66241fdede2c774345396870814b5decc2
sharadbhat <sharad.mbhat@gmail.com>
2020-11-06 19:28:46 +0530

    4 pages

--- client/src/pages/subpages/workloads/DaemonsetsList.jsx
+++ client/src/pages/subpages/workloads/DaemonsetsList.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { message, Tag, Tooltip, Collapse } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { v4 as uuid } from "uuid";
+
+// Components
+import DataTable from "../../../components/DataTable";
 
 // Utils
 import { Context } from "../../../utils/Context";
@@ -6,14 +13,122 @@ import { Context } from "../../../utils/Context";
 class DaemonsetsList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [],
+    };
+
+    this.columns = [
+      {
+        title: "Name",
+        dataIndex: ["metadata", "name"],
+        key: uuid(),
+        sorter: (a, b) => a.metadata.name.localeCompare(b.metadata.name),
+        sortDirections: ["descend", "ascend"],
+      },
+      {
+        title: "Labels",
+        dataIndex: ["metadata", "labels"],
+        key: uuid(),
+        render: (labelObject) => {
+          let labelList = [];
+          for (const label in labelObject) {
+            if (labelObject.hasOwnProperty(label)) {
+              labelList.push(`${label}: ${labelObject[label]}`);
+            }
+          }
+
+          if (labelList.length > 0) {
+            return (
+              <Collapse key={uuid()}>
+                <Collapse.Panel header="View">
+                  {labelList.map((label) => {
+                    return <Tag>{label}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+      {
+        title: "Age",
+        dataIndex: ["metadata", "creationTimestamp"],
+        key: uuid(),
+        sorter: (a, b) =>
+          moment(b.metadata.creationTimestamp) -
+          moment(a.metadata.creationTimestamp),
+        sortDirections: ["descend", "ascend"],
+        render: (creationTimestamp) => {
+          return (
+            <Tooltip
+              title={moment(creationTimestamp).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(creationTimestamp).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+      {
+        title: "Images",
+        dataIndex: ["spec", "template", "spec", "containers"],
+        key: uuid(),
+        render: (containersList) => {
+          let imageList = [];
+          containersList.forEach((container) => {
+            imageList.push(container.image);
+          });
+
+          if (imageList.length > 0) {
+            return (
+              <Collapse key={uuid()}>
+                <Collapse.Panel header="View">
+                  {imageList.map((image) => {
+                    return <Tag>{image}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+    ];
   }
 
   componentDidMount = () => {
     this.context.setHeader("Daemon Sets");
+    this.getDaemonSets();
+  };
+
+  getDaemonSets = async () => {
+    try {
+      let serverResponse = await axios.get(
+        `/workload/${this.context.state.namespace}/get-daemon-sets`
+      );
+      if (serverResponse.status === 200) {
+        this.setState({
+          data: serverResponse.data.response.body.daemonSets,
+        });
+      } else {
+        console.log("Error occurred");
+        message.error("Error occurred");
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+      message.error("Error occurred");
+    }
   };
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <DataTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
   }
 }
 
--- client/src/pages/subpages/workloads/PodsList.jsx
+++ client/src/pages/subpages/workloads/PodsList.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { message, Tag, Tooltip, Collapse, Popover, Descriptions } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { v4 as uuid } from "uuid";
+
+// Components
+import DataTable from "../../../components/DataTable";
 
 // Utils
 import { Context } from "../../../utils/Context";
@@ -6,14 +13,123 @@ import { Context } from "../../../utils/Context";
 class PodsList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [],
+    };
+
+    this.statusColors = {
+      Pending: "orange",
+      Running: "green",
+      Succeeded: "blue",
+      Failed: "red",
+      Unknown: "orange",
+    };
+
+    this.columns = [
+      {
+        title: "Name",
+        dataIndex: ["metadata", "name"],
+        key: uuid(),
+        sorter: (a, b) => a.metadata.name.localeCompare(b.metadata.name),
+        sortDirections: ["descend", "ascend"],
+      },
+      {
+        title: "Namespace",
+        dataIndex: ["metadata", "namespace"],
+        key: uuid(),
+      },
+      {
+        title: "Labels",
+        dataIndex: ["metadata", "labels"],
+        key: uuid(),
+        render: (labelObject) => {
+          let labelList = [];
+          for (const label in labelObject) {
+            if (labelObject.hasOwnProperty(label)) {
+              labelList.push(`${label}: ${labelObject[label]}`);
+            }
+          }
+
+          if (labelList.length > 0) {
+            return (
+              <Collapse key={uuid()}>
+                <Collapse.Panel header="View">
+                  {labelList.map((label) => {
+                    return <Tag>{label}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+      {
+        title: "Node",
+        dataIndex: ["spec", "nodeName"],
+        key: uuid(),
+      },
+      {
+        title: "Status",
+        dataIndex: ["status", "phase"],
+        key: uuid(),
+        render: (status) => {
+          return <Tag color={this.statusColors[status]}>{status}</Tag>;
+        },
+      },
+      {
+        title: "Age",
+        dataIndex: ["metadata", "creationTimestamp"],
+        key: uuid(),
+        sorter: (a, b) =>
+          moment(b.metadata.creationTimestamp) -
+          moment(a.metadata.creationTimestamp),
+        sortDirections: ["descend", "ascend"],
+        render: (creationTimestamp) => {
+          return (
+            <Tooltip
+              title={moment(creationTimestamp).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(creationTimestamp).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+    ];
   }
 
   componentDidMount = () => {
     this.context.setHeader("Pods");
+    this.getPods();
+  };
+
+  getPods = async () => {
+    try {
+      let serverResponse = await axios.get(
+        `/workload/${this.context.state.namespace}/get-pods`
+      );
+      if (serverResponse.status === 200) {
+        this.setState({
+          data: serverResponse.data.response.body.pods,
+        });
+      } else {
+        console.log("Error occurred");
+        message.error("Error occurred");
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+      message.error("Error occurred");
+    }
   };
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <DataTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
   }
 }
 
