1cf38b17f03575a0914f61f11170606ee9775f16
sharadbhat <sharad.mbhat@gmail.com>
2020-11-10 02:12:32 +0530

    Secrets page

---
+++ client/src/pages/subpages/config/Secrets.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { message, Tag, Tooltip, Collapse } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { v4 as uuid } from "uuid";
+
+// Components
+import DataTable from "../../../components/DataTable";
+
+// Utils
+import { Context } from "../../../utils/Context";
+
+class Secrets extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+
+    this.columns = [
+      {
+        title: "Name",
+        dataIndex: ["metadata", "name"],
+        key: uuid(),
+        sorter: (a, b) => a.metadata.name.localeCompare(b.metadata.name),
+        sortDirections: ["descend", "ascend"],
+      },
+      {
+        title: "Namespace",
+        dataIndex: ["metadata", "namespace"],
+        key: uuid(),
+      },
+      {
+        title: "Labels",
+        dataIndex: ["metadata", "labels"],
+        key: uuid(),
+        render: (labelObject) => {
+          let labelList = [];
+          for (const label in labelObject) {
+            if (labelObject.hasOwnProperty(label)) {
+              labelList.push(`${label}: ${labelObject[label]}`);
+            }
+          }
+
+          if (labelList.length > 0) {
+            return (
+              <Collapse key={uuid()}>
+                <Collapse.Panel header="View">
+                  {labelList.map((label) => {
+                    return <Tag>{label}</Tag>;
+                  })}
+                </Collapse.Panel>
+              </Collapse>
+            );
+          } else {
+            return "-";
+          }
+        },
+      },
+      {
+        title: "Type",
+        dataIndex: ["type"],
+        key: uuid(),
+      },
+      {
+        title: "Age",
+        dataIndex: ["metadata", "creationTimestamp"],
+        key: uuid(),
+        sorter: (a, b) =>
+          moment(b.metadata.creationTimestamp) -
+          moment(a.metadata.creationTimestamp),
+        sortDirections: ["descend", "ascend"],
+        render: (creationTimestamp) => {
+          return (
+            <Tooltip
+              title={moment(creationTimestamp).format("MMM D, YYYY, h:mm:ss A")}
+            >
+              {moment(creationTimestamp).fromNow()}
+            </Tooltip>
+          );
+        },
+      },
+    ];
+  }
+
+  componentDidMount = () => {
+    this.context.setHeader("Secrets");
+    this.getSecrets();
+  };
+
+  getSecrets = async () => {
+    try {
+      let serverResponse = await axios.get(
+        `/api/config/${this.context.state.namespace}/get-secrets`
+      );
+      if (serverResponse.status === 200) {
+        this.setState({
+          data: serverResponse.data.response.body.secrets,
+        });
+      } else {
+        message.error("Error occured");
+      }
+    } catch (err) {
+      console.log("Error:", err);
+      message.error("Error occurred");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <DataTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
+  }
+}
+
+Secrets.contextType = Context;
+
+export default Secrets;
