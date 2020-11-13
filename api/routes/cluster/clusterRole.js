706988ddf15aaf582c04caeb746cb931f08bc744
sharadbhat <sharad.mbhat@gmail.com>
2020-11-14 02:24:36 +0530

    read apis

--- api/routes/cluster/clusterRole.js
+++ api/routes/cluster/clusterRole.js
@@ -1,13 +1,16 @@
 var express = require("express");
+const { checkSchema, validationResult } = require("express-validator");
 
 // Utils
 var rbacAuthorizationV1API = require("../../utils/kube").getRbacAuthorizationV1API();
+
+var formatErrors = require("../../utils/formatErrors");
 
 var router = express.Router();
 
 router.get("/get-cluster-roles", async function (req, res, next) {
   try {
-    let k8sResponse = await rbacAuthorizationV1API.listRoleForAllNamespaces();
+    let k8sResponse = await rbacAuthorizationV1API.listClusterRole();
     let roles = [];
     k8sResponse.body.items.map((item, i) => {
       roles.push(item);
@@ -32,5 +35,52 @@ router.get("/get-cluster-roles", async function (req, res, next) {
     });
   }
 });
+
+router.get(
+  "/read-cluster-role",
+  checkSchema({
+    name: {
+      in: ["query"],
+      isString: true,
+    },
+  }),
+  async function (req, res, next) {
+    try {
+      const errors = validationResult(req);
+      let errorList = formatErrors(errors);
+
+      if (errorList.length > 0) {
+        return res.status(400).json({
+          response: {
+            statusCode: 400,
+            body: {
+              errorMessages: errorList,
+            },
+          },
+        });
+      }
+
+      let k8sResponse = await rbacAuthorizationV1API.readClusterRole(
+        req.query.name
+      );
+
+      res.status(200).json({
+        response: {
+          statusCode: 200,
+          body: k8sResponse.body,
+        },
+      });
+    } catch (err) {
+      res.status(500).json({
+        response: {
+          statusCode: 500,
+          body: {
+            errorMessages: ["Internal Server Error"],
+          },
+        },
+      });
+    }
+  }
+);
 
 module.exports = router;
--- api/routes/cluster/namespace.js
+++ api/routes/cluster/namespace.js
@@ -69,4 +69,49 @@ router.get(
   }
 );
 
+router.get(
+  "/read-namespace",
+  checkSchema({
+    name: {
+      in: ["query"],
+      isString: true,
+    },
+  }),
+  async function (req, res, next) {
+    try {
+      const errors = validationResult(req);
+      let errorList = formatErrors(errors);
+
+      if (errorList.length > 0) {
+        return res.status(400).json({
+          response: {
+            statusCode: 400,
+            body: {
+              errorMessages: errorList,
+            },
+          },
+        });
+      }
+
+      let k8sResponse = await coreV1API.readNamespace(req.query.name);
+
+      res.status(200).json({
+        response: {
+          statusCode: 200,
+          body: k8sResponse.body,
+        },
+      });
+    } catch (err) {
+      res.status(500).json({
+        response: {
+          statusCode: 500,
+          body: {
+            errorMessages: ["Internal Server Error"],
+          },
+        },
+      });
+    }
+  }
+);
+
 module.exports = router;
