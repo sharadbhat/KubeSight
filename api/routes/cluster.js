a480a919639b10f1cfdee049496119c67a0a46ee
sharadbhat <sharad.mbhat@gmail.com>
2020-12-14 15:22:39 +0530

    event api

--- api/routes/cluster.js
+++ api/routes/cluster.js
@@ -2,12 +2,14 @@ var express = require("express");
 
 var router = express.Router();
 
+var clusterEvents = require("./cluster/clusterEvent");
 var namespaceRoutes = require("./cluster/namespace");
 var nodeRoutes = require("./cluster/node");
 var persistentVolumeRoutes = require("./cluster/persistentVolume");
 var clusterRoleRoutes = require("./cluster/clusterRole");
 var storageClassRoutes = require("./cluster/storageClass");
 
+router.use(clusterEvents);
 router.use(namespaceRoutes);
 router.use(nodeRoutes);
 router.use(persistentVolumeRoutes);
